92fc7b65f4a2d6472af3d8586fbe86a674a23d6c
Jaden Lindner <jadenlindner@ufl.edu>
2023-01-31 16:16:40 -0500

    added Token and StringLitToken classes
    tried to do token classes, not completely understanding so waiting to start Scanner instead

---
+++ edu/ufl/cise/plcsp23/StringLitToken.java
@@ -0,0 +1,23 @@
+package edu.ufl.cise.plcsp23;
+
+public class StringLitToken implements IStringLitToken {
+
+    String getValue() {
+
+    }
+
+    @Override
+    public SourceLocation getSourceLocation() {
+
+    }
+
+    @Override
+    public Kind getKind() {
+        return Kind.STRING_LIT;
+    }
+
+    @Override
+    public String getTokenString() {
+
+    }
+}
---
+++ edu/ufl/cise/plcsp23/Token.java
@@ -0,0 +1,34 @@
+package edu.ufl.cise.plcsp23;
+
+public class Token implements IToken {
+
+    final Kind kind;
+    final int line;
+    final int column;
+    final int length;
+    final String source;
+
+
+    public Token(Kind kind, int line, int column, int length, String source) {
+        super();
+        this.kind = kind;
+        this.line = line;
+        this.column = column;
+        this.length = length;
+        this.source = source;
+    }
+
+    public SourceLocation getSourceLocation() {
+        return
+    }
+
+    public Kind getKind() {
+        return kind;
+    }
+
+    public String getTokenString() {
+        return source;
+    }
+
+    //@Override public String toString() {}
+}
