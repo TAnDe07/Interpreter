9dc7c1394a634915cabe6226aea6d8d4661d995d
TAnDe07 <tdesir1@ufl.edu>
2023-02-02 18:11:49 -0500

    updated scanner

--- edu/ufl/cise/plcsp23/NumLitToken.java
+++ edu/ufl/cise/plcsp23/NumLitToken.java
@@ -2,26 +2,7 @@ package edu.ufl.cise.plcsp23;
 
 public class NumLitToken {
 
-    public NumLitToken(IToken.Kind kind, int line, int column, int length, String source) {
-        super();
-        this.kind = kind;
-        this.line = line;
-        this.column = column;
-        this.length = length;
-        this.source = source;
-    }
 
-    public INumLitToken.SourceLocation getSourceLocation() {
-        return null;
-    }
-
-    public INumLitToken.Kind getKind() {
-        return kind;
-    }
-
-    public String getNumLitString() {
-        return source;
-    }
 }
 
 
